16b332fe0c9f3c1d24d30d9dd4feb6203e55e836
Vakosik <94841803+Vakosik@users.noreply.github.com>
2022-10-04 15:00:47 +0200

    Update automatic_train.py

--- automatic_train.py
+++ automatic_train.py
@@ -9,8 +9,10 @@ import random
 
 parser = argparse.ArgumentParser()
 parser.add_argument("model", type=str, help="A path to a model that you want to fine-tune.")
-parser.add_argument("data", type=str,  help="A directory with data. The directory must contain two folders train and test. Both these folders must contain two subfolders input and target.")
+parser.add_argument("data", type=str,
+                    help="A directory with data. The directory must contain two folders train and test. Both these folders must contain two subfolders input and target.")
 args = parser.parse_args()
+
 
 def get_filenames(path):
     names = []
@@ -21,13 +23,27 @@ def get_filenames(path):
 
 def get_dataset(image_file, path):
     images = []
-    for name in image_file:
-        file_name = path+name
+    if path.endswith('input/'):
+        global random_height
+        global random_width
+        random_height = []
+        random_width = []
+    for j, name in enumerate(image_file):
+        file_name = path + name
         img = cv2.imread(file_name, flags=cv2.IMREAD_GRAYSCALE)
-        img = np.atleast_3d(img) # we need dim (128,128,1) not just (128,128)
+        img = np.atleast_3d(img)  # we need dim (128,128,1) not just (128,128)
+
+        if path.endswith('train/input/'):
+            random_height.append(random.randrange(img.shape[0] - 512))
+            random_width.append(random.randrange(img.shape[1] - 512))
+
         if path.endswith('target/'):  # masks
-            img = img/255
+            img = img / 255
         images.append(img)
+        # print(random_height[j], random_width[j], name)
+    if path.endswith('train/input/') or path.endswith('train/target/'):
+        for j in range(len(images)):
+            images[j] = images[j][random_height[j]:random_height[j] + 512, random_width[j]:random_width[j] + 512, :]
     return images
 
 
@@ -49,6 +65,7 @@ def generate(X, y, data_len, pathx, pathy, batchsize=1):
 
 
 def main(model_path, data_path):
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
     if model_path[-3:] != '.h5':
         print('Error: The path to a model does not end with .h5')
         return
@@ -59,7 +76,8 @@ def main(model_path, data_path):
     y_trn_path = data_path + 'train' + '/target/'
     X_val_path = data_path + 'test' + '/input/'
     y_val_path = data_path + 'test' + '/target/'
-    if os.path.isdir(X_trn_path) is False or os.path.isdir(y_trn_path) is False or os.path.isdir(X_val_path) is False or os.path.isdir(y_val_path) is False:
+    if os.path.isdir(X_trn_path) is False or os.path.isdir(y_trn_path) is False or os.path.isdir(
+            X_val_path) is False or os.path.isdir(y_val_path) is False:
         print('Error: Data directory does not contain required folders. Check help for the second parameter.')
         return
     X_trn = get_filenames(X_trn_path)
@@ -70,7 +88,6 @@ def main(model_path, data_path):
     y_trn.sort()
     X_val.sort()
     y_val.sort()
-
     model = load_model(model_path)
     model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
 
@@ -79,7 +96,7 @@ def main(model_path, data_path):
 
     # Saving the model that behaves better in validation set:
     callbacks = [
-        EarlyStopping(monitor="val_loss", patience=20, verbose=1, restore_best_weights=True),
+        EarlyStopping(monitor="val_loss", patience=50, verbose=1, restore_best_weights=True),
         # ModelCheckpoint('models/u_net.hdf5', monitor='val_loss', save_best_only=True, verbose=1)
     ]
 
@@ -89,10 +106,10 @@ def main(model_path, data_path):
     batch_size = 6
 
     history = model.fit(generate(X_trn, y_trn, size_train, X_trn_path, y_trn_path, batch_size),
-                                  steps_per_epoch=size_train / batch_size, epochs=200, verbose=1,
-                                  callbacks=callbacks,
-                                  validation_data=generate(X_val, y_val, size_val, X_val_path, y_val_path, 1),
-                                  validation_steps=size_val / 1)
+                        steps_per_epoch=size_train / batch_size, epochs=600, verbose=1,
+                        callbacks=callbacks,
+                        validation_data=generate(X_val, y_val, size_val, X_val_path, y_val_path, 1),
+                        validation_steps=size_val / 1)
 
     save_model_path = os.path.dirname(model_path)
     save_model_path = save_model_path + '/new_model.h5'
